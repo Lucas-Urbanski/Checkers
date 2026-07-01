af52cbbeaed405a282532da78a9b230ab32df56c
Lucas Urbanski <lcurbanski8@gmail.com>
2026-06-30 19:35:33 -0600

    board component

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import "@/styles/square.css";
-import "@/styles/piece.css";
+import Board, { PieceValue, BoardState } from "../components/board";
 
-type PieceValue = "dark" | "light" | null;
-type BoardState = PieceValue[][];
+type Player = "light" | "dark";
 
 const DEFAULT_BOARD: BoardState = [
   [null, "dark", null, "dark", null, "dark", null, "dark"],
@@ -18,44 +16,31 @@ const DEFAULT_BOARD: BoardState = [
   ["light", null, "light", null, "light", null, "light", null],
 ];
 
-function renderPiece(pieceType: PieceValue) {
-  if (pieceType === "dark") return <button className="darkPiece" />;
-  if (pieceType === "light") return <button className="lightPiece" />;
-  
-  return null;
-}
 
-function renderSquares(board: BoardState) {
-  const squares = [];
-
-  for (let row = 0; row < 8; row++) {
-    for (let col = 0; col < 8; col++) {
-      const key = `${row}-${col}`;
-      const isDarkSquare = (row + col) % 2 !== 0;
-
-      if (isDarkSquare) {
-        squares.push(
-          <div key={key} className="darkSquare">
-            {renderPiece(board[row][col])}
-          </div>
-        );
-      }
-      else {
-        squares.push(<div key={key} className="lightSquare" />);
-      }
-    }
-  }
-  return squares;
-}
 
 export default function Game() {
   const [board, setBoard] = useState<BoardState>(DEFAULT_BOARD);
+  const [selected, setSelected] = useState<[number, number] | null>(null);
+  const [validMoves, setValidMoves] = useState<[number, number][]>([]);
+  const [turn, setTurn] = useState<Player>("light");
+
+  function getValidMoves(){
+    const moves: [number, number][] = [];
+    return;
+  }
+
+  function handleSquareClick(){
+    return;
+  }
 
   return (
     <div className="flex justify-center items-center w-full h-screen bg-neutral-800">
-      <div className="grid grid-cols-8 grid-rows-8 w-[480px] h-[480px] border-2 border-neutral-600">
-        {renderSquares(board)}
-      </div>
+      <Board
+        board={board}
+        selected={selected}
+        validMoves={validMoves}
+        onSquareClick={handleSquareClick}
+      />
     </div>
   );
 }
